3393448c1c837da7fbd75376f13bfd05e70589fd
Abu <104144116+a6uxa4@users.noreply.github.com>
2023-12-18 19:34:01 +0600

    animation service card;

--- src/components/services/SubServicesCard.tsx
+++ src/components/services/SubServicesCard.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export const SubServicesCard = ({ subService }: any) => {
   return (
-    <div className="group w-[180px] h-[260px] rounded-lg bg-[#f3fbfb] flex flex-col items-center justify-around px-3 py-3 gap-2 cursor-pointer hover:bg-[#01d6d4] duration-700">
+    <div className="group w-[180px] h-[270px] rounded-lg bg-[#f3fbfb] flex flex-col items-center justify-around px-3 py-3 gap-2 cursor-pointer hover:bg-[#01d6d4] duration-700">
       <div className="w-[120px] h-[120px] min-h-[120px] min-w-[120px] flex items-center justify-center">
         <div className="flex w-full h-full items-center justify-center group-hover:hidden">
           <Image src={subService.icon} alt="icon" />
@@ -11,10 +11,15 @@ export const SubServicesCard = ({ subService }: any) => {
           <Image src={subService.photo} alt="photo" />
         </div>
       </div>
-      <div className="w-full h-full text-sm text-center group-hover:text-white">
+      <div className="w-full h-full max-h-[43%] min-h-[43%] text-sm text-center group-hover:text-white">
         {subService.name}
       </div>
-      <div className="w-fit px-2 py-2 bg-[#01e0de] skew-y-3">Подробнее</div>
+      <button
+        style={{ transform: "skew(-14deg)" }}
+        className="bg-[#01e0de] px-[15px] rounded-md text-white py-[5px] text-[14px] font-semibold cursor-pointer relative bottom-[-11px] hidden group-hover:block hover:bg-[#14918f]"
+      >
+        Подробнее
+      </button>
     </div>
   );
 };
