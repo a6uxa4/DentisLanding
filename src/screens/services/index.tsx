d5b3a21f5170d90d010eb83a3ad570276f9f6a89
Abu <104144116+a6uxa4@users.noreply.github.com>
2023-12-18 19:11:00 +0600

    animation

--- src/screens/services/index.tsx
+++ src/screens/services/index.tsx
@@ -1,10 +1,25 @@
+import React, { useState } from "react";
 import { ServicesCard } from "@/components/services/ServicesCard";
 import { SubServicesCard } from "@/components/services/SubServicesCard";
 import { SERVICES_DATA } from "@/utils/constants/services.constant";
-import React, { useState } from "react";
+import { motion } from "framer-motion";
 
 export const ServicesPage = () => {
   const [activeService, setActiveService] = useState(1);
+
+  const fadeInAnimationVariants = {
+    initial: {
+      opacity: 0,
+      y: 100,
+    },
+    animate: (index: number) => ({
+      opacity: 1,
+      y: 0,
+      transition: {
+        delay: 0.05 * index,
+      },
+    }),
+  };
   return (
     <div className="w-full flex flex-col">
       <div className="flex w-full gap-4">
@@ -18,8 +33,19 @@ export const ServicesPage = () => {
         ))}
       </div>
       <div className="flex">
-        {SERVICES_DATA[activeService - 1].subServices.map((item) => (
-          <SubServicesCard key={item.name} subService={item} />
+        {SERVICES_DATA[activeService - 1].subServices.map((item, index) => (
+          <motion.div
+            key={index}
+            variants={fadeInAnimationVariants}
+            initial="initial"
+            whileInView="animate"
+            viewport={{
+              once: true,
+            }}
+            custom={index}
+          >
+            <SubServicesCard subService={item} />
+          </motion.div>
         ))}
       </div>
     </div>
