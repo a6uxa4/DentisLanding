07ecc3574db9f11c7342b9982e98a9e721b67456
Abu <104144116+a6uxa4@users.noreply.github.com>
2023-12-22 04:52:23 +0600

    final banner

--- src/screens/banner/index.tsx
+++ src/screens/banner/index.tsx
@@ -1,16 +1,86 @@
-import React from "react";
-const videoSrc = require("../../assets/video/EsteticVideo.mp4").default;
+import { Typewriter } from "react-simple-typewriter";
+import BannerPhoto from "../../assets/images/Banner.png";
+import Image from "next/image";
+import { motion } from "framer-motion";
 
 export const BannerPage = () => {
+  const fadeInAnimationVariants = {
+    initial: {
+      opacity: 0,
+      y: 300,
+    },
+    animate: () => ({
+      opacity: 1,
+      y: 0,
+      transition: {
+        delay: 0.05,
+      },
+    }),
+  };
+
+  const fadeAnimationVariants = {
+    initial: {
+      opacity: 0,
+      y: -300,
+    },
+    animate: () => ({
+      opacity: 1,
+      y: 0,
+      transition: {
+        delay: 0.05,
+      },
+    }),
+  };
+
   return (
-    <div className="h-[700px] max-h-[700px] relative">
-      <video
-        src={videoSrc}
-        autoPlay
-        loop
-        muted
-        className="w-full h-full object-cover bg-[rgb(0,0,0,.5)]"
-      />
+    <div className="w-full h-full flex justify-between items-center gap-28 border-b pb-[50px]">
+      <div className="w-full">
+        <div className="bg-[#d7fffe] absolute bottom-2 -z-10  h-[500px] w-[35.25rem] rounded-full blur-[10rem]"></div>
+        <div className="w-full min-w-[650px] max-w-[650px] font-mono font-black text-[40px] leading-[50px] tracking-wide">
+          <Typewriter
+            words={[
+              "Превосходный опыт",
+              "Доверьте свою улыбку экспертам",
+              "Радость здоровой улыбки - наша цель в",
+              "Искусство здоровой улыбки начинается здесь, в нашей",
+              "Качество заботы о вашей улыбке - наша страсть к",
+            ]}
+            loop={5}
+            cursor
+            cursorStyle="_"
+            typeSpeed={100}
+            deleteSpeed={50}
+            delaySpeed={5000}
+          />
+          <span className="text-[#00d6d4]">стомотологии</span>
+        </div>
+      </div>
+      <div className="w-full h-full relative flex items-center justify-center ">
+        <motion.div
+          className="w-[500px] h-[500px] rounded-full bg-gradient-to-b from-[#d7fffe] via-[#40fffc] to-[#00d6d4] shadow-xl"
+          variants={fadeInAnimationVariants}
+          initial="initial"
+          whileInView="animate"
+          viewport={{
+            once: true,
+          }}
+        ></motion.div>
+        <motion.div
+          className="w-[350px] h-[500px] absolute top-0"
+          variants={fadeAnimationVariants}
+          initial="initial"
+          whileInView="animate"
+          viewport={{
+            once: true,
+          }}
+        >
+          <Image
+            src={BannerPhoto}
+            alt="BannerPhoto"
+            className="w-[350px] h-[500px]"
+          />
+        </motion.div>
+      </div>
     </div>
   );
 };
