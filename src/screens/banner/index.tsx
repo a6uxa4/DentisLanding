e19406cef90318509b29d045d852f66b19f28bf2
Abu <104144116+a6uxa4@users.noreply.github.com>
2023-12-21 06:27:59 +0600

    fixg

--- src/screens/banner/index.tsx
+++ src/screens/banner/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 const videoSrc = require("../../assets/video/EsteticVideo.mp4").default;
 
+
 export const BannerPage = () => {
   return (
     <div className="h-[700px] max-h-[700px] relative">
