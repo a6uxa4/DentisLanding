c0b8f9da868b9a2483ff1a9987d6c973a8b97785
Abu <104144116+a6uxa4@users.noreply.github.com>
2023-12-24 04:14:56 +0600

    save

--- src/components/personal/PersonalCard.tsx
+++ src/components/personal/PersonalCard.tsx
@@ -5,7 +5,7 @@ export const PersonalCard = ({ personal }: any) => {
   return (
     <div className="group w-[300px] h-[400px] relative flex flex-col items-start justify-between cursor-pointer m-2.5">
       <div className="w-full h-full flex items-end justify-center">
-        <div className="w-[250px] h-[120px] bg-[#EEF9F9] group-hover:bg-[#00d6d4] duration-500 rounded-xl absolute z-[2] border-none"></div>
+        <div className="w-[250px] h-[120px] bg-[#EEF9F9] group-hover:bg-[#00d6d4] duration-500 rounded-xl absolute z-[2]"></div>
         <Image src={personal.photo} alt="personal" className="z-[9] absolute" />
       </div>
       <div className="w-full h-full flex flex-col items-center mt-2">
