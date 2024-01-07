3a4939dd6ecd6d6b512f8d4a085911c9cf129ca6
Abu <104144116+a6uxa4@users.noreply.github.com>
2024-01-07 20:37:56 +0600

    remove

--- src/screens/appointments/index.tsx
+++ src/screens/appointments/index.tsx
@@ -28,8 +28,6 @@ export const Appointments = () => {
     }
   };
 
-  // this is my appointments for patients online appointmentsre
-
   return (
     <div className="w-[100vw] bg-[#eef9f9] relative flex justify-center items-start py-10 text-center">
       <div className="w-full flex flex-col items-center justify-between gap-2 max-w-[1440px]">
